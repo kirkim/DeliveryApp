b72bb53f38daedbcf6308ab1eb3eb5053d2b6a37
kimkirim <kirimrlfla@naver.com>
2022-02-11 19:45:17 +0900

    s

---
+++ kirkim_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kirkim_App
+//
+//  Created by 김기림 on 2022/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
